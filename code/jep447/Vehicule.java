41a5302efacb08a6e08ae41b797b85ba47f03ec3
Clem-mal <102059689+Clem-mal@users.noreply.github.com>
2024-12-18 21:10:23 +0000

    Deploying to gh-pages from @ Clem-mal/java-talk@cce0ae7d99444d01f30a7c93fa068d37f4409a8e 🚀

--- code/jep447/Vehicule.java
+++ code/jep447/Vehicule.java
@@ -1,5 +1,7 @@
 package code.jep447;
 
+import java.util.function.BiFunction;
+
 public class Vehicule {
 
     public void mouvement() {
@@ -9,4 +11,15 @@ public class Vehicule {
     public boolean vitesseMax(int vitesse) {
         return vitesse > 300;
     }
+
+    public String check(Object o) {
+        return switch (o) {
+            case Integer i -> "C'est un entier";
+            case Float f -> "C'est un nombre à virgule";
+            case Voiture voiture -> "C'est un objet Car";
+            default -> "Je ne sais pas ce que c'est";
+        };
+    }
+
+    BiFunction<Integer, String, Integer> processAOnly = (a, b) -> a * 2;
 }
