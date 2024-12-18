cce0ae7d99444d01f30a7c93fa068d37f4409a8e
clement <clement.mallambic@laposte.net>
2024-12-18 22:09:34 +0100

    comiit

--- code/Application.java
+++ code/Application.java
@@ -1,6 +1,40 @@
 package code;
 
+import code.jep447.Voiture;
+
+/// Returns a hash code value for the object. This method is
+/// supported for the benefit of hash tables such as those provided by
+/// [java.util.HashMap].
+///
+/// The general contract of `hashCode` is:
+///
+///   - Whenever it is invoked on the same object more than once during
+///     an execution of a Java application, the `hashCode` method
+///     must consistently return the same integer, provided no information
+///     used in `equals` comparisons on the object is modified.
+///     This integer need not remain consistent from one execution of an
+///     application to another execution of the same application.
+///   - If two objects are equal according to the
+///     [equals][#equals(Object)] method, then calling the
+///     `hashCode` method on each of the two objects must produce the
+///     same integer result.
+///   - It is _not_ required that if two objects are unequal
+///     according to the [equals][#equals(Object)] method, then
+///     calling the `hashCode` method on each of the two objects
+///     must produce distinct integer results.  However, the programmer
+///     should be aware that producing distinct integer results for
+///     unequal objects may improve the performance of hash tables.
+///
+/// @implSpec
+/// As far as is reasonably practical, the `hashCode` method defined
+/// by class `Object` returns distinct integers for distinct objects.
+///
+/// @return  a hash code value for this object.
+/// @see     java.lang.Object#equals(java.lang.Object)
+/// @see     java.lang.System#identityHashCode
+
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Gatherer;
 
 public class Application {
@@ -51,6 +85,12 @@ public class Application {
                 (_, _) -> {
                 }
         );
+        BiFunction<Integer, String, Integer> processAOnly = (a, _) -> a * 2;
+
+
+    }
+
+
     }
 
 
@@ -65,6 +105,15 @@ public class Application {
         System.out.println(result);
     }
 
+    public String check(Object o) {
+        return switch(o) {
+            case int i -> "C'est un entier";
+            case float  s -> "C'est un nombre à virgule";
+            case Car c -> "C'est un objet Car";
+            default -> "Je ne sais pas ce que c'est";
+        };
+    }
+
 
 
 
@@ -75,3 +124,35 @@ public class Application {
             .gather(limit())
             .toList();*/
 }
+
+
+/// Returns a hash code value for the object. This method is
+/// supported for the benefit of hash tables such as those provided by
+/// [java.util.HashMap].
+///
+/// The general contract of `hashCode` is:
+///
+///   - Whenever it is invoked on the same object more than once during
+///     an execution of a Java application, the `hashCode` method
+///     must consistently return the same integer, provided no information
+///     used in `equals` comparisons on the object is modified.
+///     This integer need not remain consistent from one execution of an
+///     application to another execution of the same application.
+///   - If two objects are equal according to the
+///     [equals][#equals(Object)] method, then calling the
+///     `hashCode` method on each of the two objects must produce the
+///     same integer result.
+///   - It is _not_ required that if two objects are unequal
+///     according to the [equals][#equals(Object)] method, then
+///     calling the `hashCode` method on each of the two objects
+///     must produce distinct integer results.  However, the programmer
+///     should be aware that producing distinct integer results for
+///     unequal objects may improve the performance of hash tables.
+///
+/// @implSpec
+/// As far as is reasonably practical, the `hashCode` method defined
+/// by class `Object` returns distinct integers for distinct objects.
+///
+/// @return  a hash code value for this object.
+/// @see     java.lang.Object#equals(java.lang.Object)
+/// @see     java.lang.System#identityHashCode
