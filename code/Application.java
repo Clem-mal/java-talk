ec6160bf59dc121a5a29abf10f0f6057ae30c5a2
Clem-mal <102059689+Clem-mal@users.noreply.github.com>
2024-12-18 00:05:49 +0000

    Deploying to gh-pages from @ Clem-mal/java-talk@3ac47b2256ec602214cd3d017fcb90f94f5c4229 🚀

--- code/Application.java
+++ code/Application.java
@@ -1,7 +1,5 @@
 package code;
 
-import org.w3c.dom.css.Counter;
-
 import java.util.List;
 import java.util.stream.Gatherer;
 
@@ -10,11 +8,14 @@ public class Application {
     static Gatherer<String, ?, String> filter() {
         return Gatherer.of((state, element, downstream) -> {
             if (element.endsWith("1")) {
-                return downstream.push(element); // -> on  propage l element a l'operation suivante
+                return downstream.push(element);
             }
-            return true; // on renvoit vrai, car les élements d'après doivent être filtrer également
+            return true;
         });
+
+        //resultat [item1, item11]
     }
+
     static Gatherer<String, ?, String> tantQue() {
         return Gatherer.of((state, element, downstream) -> {
             if (element.startsWith("item")) {
@@ -27,9 +28,10 @@ public class Application {
 
     static Gatherer<String, ?, String> limit() {
 
-        class Counter{
+        class Counter {
             int counter;
-            Counter(int counter){
+
+            Counter(int counter) {
                 this.counter = counter;
             }
         }
@@ -38,35 +40,38 @@ public class Application {
                 () -> new Counter(0),
                 (state, element, downstream) -> {
                     //integrator
-            if (state.counter++ == 3) {
-                return false;
-            }
-            return downstream.push(element);
-        },
+                    if (state.counter++ == 3) {
+                        return false;
+                    }
+                    return downstream.push(element);
+                },
                 //combiner
-                (c1, c2) -> new Counter(c1.counter + c2.counter ),
+                (c1, c2) -> new Counter(c1.counter + c2.counter),
                 //finisher
-                (_,_) ->  {}
+                (_, _) -> {
+                }
         );
     }
-
-
 
 
     public static void main(String[] args) {
 
         List<String> texts = List.of("item1", "item2", "otem3", "item11");
 
-        /*var result = texts.stream()
-                .filter(s -> s.endsWith("1"))
-                .toList();*/
-
         var result = texts.stream()
-              /*  .gather(filter())*/
-                .gather(tantQue())
-               /* .gather(limit())*/
+                .gather(filter())
                 .toList();
 
         System.out.println(result);
     }
+
+
+
+
+
+/*    var result = texts.stream()
+            .gather(filter())
+            .gather(tantQue())
+            .gather(limit())
+            .toList();*/
 }
