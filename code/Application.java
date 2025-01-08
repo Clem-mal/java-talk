246c38a36b89fdb008a3d7505d9a4f3cacad9d78
clement <clement.mallambic@laposte.net>
2025-01-08 23:11:38 +0100

    comiit

--- code/Application.java
+++ code/Application.java
@@ -2,43 +2,42 @@ package code;
 
 import code.jep447.Voiture;
 
-/// Returns a hash code value for the object. This method is
-/// supported for the benefit of hash tables such as those provided by
-/// [java.util.HashMap].
-///
-/// The general contract of `hashCode` is:
-///
-///   - Whenever it is invoked on the same object more than once during
-///     an execution of a Java application, the `hashCode` method
-///     must consistently return the same integer, provided no information
-///     used in `equals` comparisons on the object is modified.
-///     This integer need not remain consistent from one execution of an
-///     application to another execution of the same application.
-///   - If two objects are equal according to the
-///     [equals][#equals(Object)] method, then calling the
-///     `hashCode` method on each of the two objects must produce the
-///     same integer result.
-///   - It is _not_ required that if two objects are unequal
-///     according to the [equals][#equals(Object)] method, then
-///     calling the `hashCode` method on each of the two objects
-///     must produce distinct integer results.  However, the programmer
-///     should be aware that producing distinct integer results for
-///     unequal objects may improve the performance of hash tables.
-///
-/// @implSpec
-/// As far as is reasonably practical, the `hashCode` method defined
-/// by class `Object` returns distinct integers for distinct objects.
-///
-/// @return  a hash code value for this object.
-/// @see     java.lang.Object#equals(java.lang.Object)
-/// @see     java.lang.System#identityHashCode
-
 import java.util.List;
-import java.util.function.BiFunction;
+import java.util.Random;
 import java.util.stream.Gatherer;
 
 public class Application {
 
+
+
+    public static void main(String[] args) {
+        List<String> artistes = List.of("Les Casseurs Flotteurs",
+                "Les Cowboys Fringuants",
+                "Hans Zimmer",
+                "The offspring");
+
+
+        var result = artistes.stream()
+                .gather(filterEndWithS())
+                .toList();
+        System.out.println(result);
+
+
+
+
+}
+
+
+    public String check(Object o) {
+        return switch(o) {
+            case Integer integer -> "C'est un entier";
+            case String  string -> "C'est un string";
+            case Voiture voiture -> "C'est un objet Car";
+            default -> "Je ne sais pas ce que c'est";
+        };
+    }
+}
+/*
     static Gatherer<String, ?, String> filter() {
         return Gatherer.of((state, element, downstream) -> {
             if (element.endsWith("1")) {
@@ -113,6 +112,7 @@ public class Application {
             default -> "Je ne sais pas ce que c'est";
         };
     }
+*/
 
 
 
@@ -123,7 +123,6 @@ public class Application {
             .gather(tantQue())
             .gather(limit())
             .toList();*/
-}
 
 
 /// Returns a hash code value for the object. This method is
